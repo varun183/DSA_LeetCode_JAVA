6b4bfc05e57c24d0351b7f41e372e91ee9c7f815
S Varun <125751714+varun183@users.noreply.github.com>
2023-03-19 19:18:37 +0530

    Update swap.java

--- Practice/Arrays/src/swap.java
+++ Practice/Arrays/src/swap.java
@@ -1,2 +1,23 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class swap {
+
+    public static void main(String[] args) {
+        int[] arr  = {1,5,7,8,9};
+
+        swap_num(arr,1,2);
+        System.out.println(Arrays.toString(arr));
+
+
+    }
+
+    static void swap_num(int[] arr, int index1, int index2){
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+
+        arr[index2] = temp;
+    }
+
 }
+
